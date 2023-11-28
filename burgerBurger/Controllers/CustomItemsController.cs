d3ab86bad970b044720a0a68de52477d79ffa56c
Michael Rosanelli <michaelrosanelli105@gmail.com>
2023-11-28 11:14:04 -0500

    editing custom burgers wasnt working for the life of me so i aint doing it

--- burgerBurger/Controllers/CustomItemsController.cs
+++ burgerBurger/Controllers/CustomItemsController.cs
@@ -96,57 +96,6 @@ namespace burgerBurger.Controllers
             return View(customBurger);
         }
 
-        // GET: CustomItems/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.CustomItem == null)
-            {
-                return NotFound();
-            }
-
-            var customItem = await _context.CustomItem.FindAsync(id);
-            if (customItem == null)
-            {
-                return NotFound();
-            }
-            return View(customItem);
-        }
-
-        // POST: CustomItems/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,totalCalories,Price,Photo")] CustomItem customItem)
-        {
-            if (id != customItem.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(customItem);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CustomItemExists(customItem.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(customItem);
-        }
-
         // GET: CustomItems/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
