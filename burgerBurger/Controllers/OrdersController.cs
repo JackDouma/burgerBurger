55e9c084539820eeb36ca6e1e547324899966d0e
Michael Rosanelli <michaelrosanelli105@gmail.com>
2023-11-30 13:11:22 -0500

    added update status field on orders for all non-customers

--- burgerBurger/Controllers/OrdersController.cs
+++ burgerBurger/Controllers/OrdersController.cs
@@ -16,21 +16,29 @@ namespace burgerBurger.Controllers
     public class OrdersController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public OrdersController(ApplicationDbContext context)
+        public OrdersController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: Orders
         public async Task<IActionResult> Index()
         {
+            var u = _userManager.Users.Where(u => u.UserName == "michaelrosanelli105@gmail.com").First();
+            await _userManager.RemoveFromRoleAsync(u, "Manager");
+            var res = await _userManager.IsInRoleAsync(u, "Manager");
+            await _userManager.AddToRoleAsync(u, "Manager");
+            res = await _userManager.IsInRoleAsync(u, "Manager");
             //var userManager = scope.service
             // if user is not an admin, show only their orders
             if (User.IsInRole("Customer"))
                 return View(await _context.Orders.Where(o => o.CustomerId == User.Identity.Name).OrderBy(o => o.OrderId).ToListAsync());
             else if (User.IsInRole("Manager")) {
                 var location = _context.Users.FirstOrDefaultAsync(u => u.UserName == User.Identity.Name).Result.locationIdentifier;
+                Response.Headers.Add("Refresh", "5");
                 return View(await _context.Orders.Where(o => o.LocationId == location).ToListAsync());
             }
             else
@@ -46,7 +54,7 @@ namespace burgerBurger.Controllers
                 return NotFound();
             }
 
-            if (User.IsInRole("Administrator"))
+            if (User.IsInRole("Administrator") || User.IsInRole("Owner"))
             {
                 var order = await _context.Orders
                     .Include(o => o.OrderDetails).ThenInclude(od => od.Item)
@@ -60,9 +68,11 @@ namespace burgerBurger.Controllers
                 ViewData["location"] = _context.Location.FirstOrDefaultAsync(l => l.LocationId == order.LocationId).Result.DisplayName;
                 return View(order);
             }
-            else
+            else if (User.IsInRole("Customer"))
             {
-                var order = await _context.Orders.Include(o => o.OrderDetails).ThenInclude(od => od.Item).Where(o => o.CustomerId == User.Identity.Name)
+                var order = await _context.Orders
+                    .Include(o => o.OrderDetails).ThenInclude(od => od.Item)
+                    .Where(o => o.CustomerId == User.Identity.Name)
                     .FirstOrDefaultAsync(m => m.OrderId == id);
                 if (order == null)
                 {
@@ -72,6 +82,41 @@ namespace burgerBurger.Controllers
                 ViewData["location"] = _context.Location.FirstOrDefaultAsync(l => l.LocationId == order.LocationId).Result.DisplayName;
                 return View(order);
             }
+            else if (User.IsInRole("Manager"))
+            {
+                var user = _context.Users.FirstOrDefaultAsync(u => u.UserName == User.Identity.Name).Result;
+                var order = await _context.Orders
+                    .Include(o => o.OrderDetails).ThenInclude(od => od.Item)
+                    .Where(o => o.LocationId == user.locationIdentifier)
+                    .FirstOrDefaultAsync(m => m.OrderId == id);
+                if (order == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                ViewData["location"] = _context.Location.FirstOrDefaultAsync(l => l.LocationId == order.LocationId).Result.DisplayName;
+                return View(order);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Manager,Owner,Admin")]
+        public async Task<IActionResult> Update(int ItemId, string Status)
+        {
+            var order = await _context.Orders.FindAsync(ItemId);
+            if(order != null)
+            {
+                order.Status = Status;
+                _context.Update(order);
+                _context.SaveChanges();
+                return RedirectToAction("Details", new { @id = order.OrderId });
+            }
+            return RedirectToAction("Index");
+
         }
 
         private bool OrderExists(int id)
