6493ad64966f856645fabbbdb939e78a72f95359
alessio <alessio.schiavi@georgebrown.ca>
2023-12-04 14:19:22 -0500

    Added user management page (WIP, no auth)

---
+++ burgerBurger/Controllers/UserManagementController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using burgerBurger.Models;
+using burgerBurger.Data;
+
+namespace burgerBurger.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public UserManagementController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var users = (from user in _userManager.Users
+                         join userRole in _context.UserRoles on user.Id equals userRole.UserId
+                         join role in _context.Roles on userRole.RoleId equals role.Id
+                         select new UserViewModel
+                         {
+                             Email = user.Email,
+                             Role = role.Name
+                         }).ToList();
+
+            return View(users);
+        }
+    }
+}
---
+++ burgerBurger/Models/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace burgerBurger.Models
+{
+    public class UserViewModel
+    {
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
