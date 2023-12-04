c51707c51686bc912f559751fccd5bdfea189df2
alessio <alessio.schiavi@georgebrown.ca>
2023-12-04 14:38:22 -0500

    Usermanagement page, change users roles, authorized only to owner/admin via account management page.

--- burgerBurger/Controllers/UserManagementController.cs
+++ burgerBurger/Controllers/UserManagementController.cs
@@ -3,17 +3,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using burgerBurger.Models;
 using burgerBurger.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
 
 namespace burgerBurger.Controllers
 {
+    [Authorize(Roles = "Admin,Owner")]
     public class UserManagementController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly ApplicationDbContext _context;
 
-        public UserManagementController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+
+        public UserManagementController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext context)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
             _context = context;
         }
 
@@ -24,11 +30,34 @@ namespace burgerBurger.Controllers
                          join role in _context.Roles on userRole.RoleId equals role.Id
                          select new UserViewModel
                          {
+                             Id = user.Id,
                              Email = user.Email,
-                             Role = role.Name
+                             CurrentRole = role.Name,
+                             AvailableRoles = _roleManager.Roles.Select(r => r.Name).ToList()
                          }).ToList();
 
             return View(users);
+        }
+
+        [HttpPost]
+        public IActionResult ChangeRole(string userId, string newRole)
+        {
+            var user = _userManager.FindByIdAsync(userId).Result;
+            var currentRoles = _userManager.GetRolesAsync(user).Result;
+
+            var removeResult = _userManager.RemoveFromRolesAsync(user, currentRoles).Result;
+            if (!removeResult.Succeeded)
+            {
+                return View("Error");
+            }
+
+            var addResult = _userManager.AddToRoleAsync(user, newRole).Result;
+            if (!addResult.Succeeded)
+            {
+                return View("Error");
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
--- burgerBurger/Models/UserViewModel.cs
+++ burgerBurger/Models/UserViewModel.cs
@@ -4,7 +4,9 @@ namespace burgerBurger.Models
 {
     public class UserViewModel
     {
+        public string Id { get; set; }
         public string Email { get; set; }
-        public string Role { get; set; }
+        public string CurrentRole { get; set; }
+        public List<string> AvailableRoles { get; set; }
     }
 }
