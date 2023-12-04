eb7bc486c486f506f4458d86c97a3794de0f6a32
alessio <alessio.schiavi@georgebrown.ca>
2023-12-04 16:02:16 -0500

    SMS notification for inventory expirey, currently static send number, need to change logic for locations and managers.

---
+++ burgerBurger/Data/Migrations/20231204205420_twilioexpiry.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace burgerBurger.Data.Migrations
+{
+    public partial class twilioexpiry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "MessageSent",
+                table: "Inventory",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MessageSent",
+                table: "Inventory");
+        }
+    }
+}
