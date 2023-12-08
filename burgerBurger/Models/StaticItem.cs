1a0c609328db9ccf211db4aa36aa5fb81d68c3ee
Michael Rosanelli <michaelrosanelli105@gmail.com>
2023-12-08 15:03:37 -0500

    make text black

--- burgerBurger/Models/StaticItem.cs
+++ burgerBurger/Models/StaticItem.cs
@@ -1,45 +1,4 @@
-﻿/*using burgerBurger.Enums;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-
-namespace burgerBurger.Models
-{
-    public class StaticItem
-    {
-
-        public int StaticItemId { get; set; }
-
-        [Required]
-        public StaticItemType Type { get; set; }
-
-        [Required]
-        [MaxLength(255)]
-        public string? Name { get; set; }
-
-        [Required]
-        [MaxLength(255)]
-        public string? Description { get; set; }
-
-        [Range(0, int.MaxValue)]
-        [DisplayName("Total Calories")]
-        public int totalCalories { get; set; }
-
-        [Required]
-        [Range(0, double.MaxValue)]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public double Price { get; set; }
-
-        public List<Inventory>? Ingredients { get; set; }
-
-        public StaticItem() { 
-            Ingredients = new List<Inventory>();
-        }
-
-    }
-}
-*/
-
-using burgerBurger.Enums;
+﻿using burgerBurger.Enums;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
